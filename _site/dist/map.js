0774d60f1426b40c1ee2b27b7d479c54fa15df2a
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-03-29 13:13:17 +0100

    first commit

---
+++ _site/dist/map.js
@@ -0,0 +1,6 @@
+$(function () {
+  $('#map').JSMaps({
+    map: 'netherlands', //Use any map as named in the maps folder e.g. usa
+    mapWidth: 300,
+  });
+});
---
+++ _site/dist/site.js
@@ -0,0 +1,19 @@
+// 
+
+let $ = jQuery.noConflict();
+
+$(document).ready(function () {
+  // // if you scroll down 20px from the top, add a class to the navbar dont use jquery
+  // window.onscroll = function () {
+  //   var logo = document.getElementById("logo");
+  //   if (window.scrollY >= 20) {
+  //     logo.classList.add("logo_small");
+  //   }
+
+  //   //console.log(window.scrollY);
+  //   if (window.scrollY == 0) {
+  //     logo.classList.remove("logo_small");
+  //   }
+  // }
+});
+
