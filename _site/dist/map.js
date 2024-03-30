8ca1847c1301d1a61a4d88f358d1a5340ddb8424
Tjerk Dijkstra <tjerk.dijkstra@icloud.com>
2024-03-30 12:07:33 +0100

    contact start

--- _site/dist/map.js
+++ _site/dist/map.js
@@ -1,6 +1,12 @@
 $(function () {
-  $('#map').JSMaps({
-    map: 'netherlands', //Use any map as named in the maps folder e.g. usa
-    mapWidth: 300,
-  });
+
+  // only if #map exists
+  if ($('#map').length === 0) {
+    return;
+  } else {
+    $('#map').JSMaps({
+      map: 'netherlands', //Use any map as named in the maps folder e.g. usa
+      mapWidth: 300,
+    });
+  }
 });
--- _site/dist/site.js
+++ _site/dist/site.js
@@ -3,18 +3,36 @@
 let $ = jQuery.noConflict();
 
 $(document).ready(function () {
-  // // if you scroll down 20px from the top, add a class to the navbar dont use jquery
-  // window.onscroll = function () {
-  //   var logo = document.getElementById("logo");
-  //   if (window.scrollY >= 20) {
-  //     logo.classList.add("logo_small");
-  //   }
 
-  //   //console.log(window.scrollY);
-  //   if (window.scrollY == 0) {
-  //     logo.classList.remove("logo_small");
-  //   }
-  // }
+  $('.contact_block').on('click', function () {
+
+    // get the data attribute of this element
+    let data = $(this).data('name');
+
+    if (data == 'question') {
+      // if its already down slide up
+      if ($('.question_form').is(':visible')) {
+        $('.question_form').slideUp(500);
+      } else {
+        $('.question_form').slideDown(500);
+      }
+    }
+
+    // find element with class arrow-up and change name to arrow-down
+    let arrow = $(this).find('.arrow-up');
+
+    if (!arrow.length) {
+      arrow = $(this).find('.arrow-down');
+    }
+
+    if (arrow.hasClass('arrow-up')) {
+      arrow.removeClass('arrow-up').addClass('arrow-down');
+      arrow.attr('name', 'arrow-down');
+    } else {
+      arrow.removeClass('arrow-down').addClass('arrow-up');
+      arrow.attr('name', 'arrow-up');
+    }
+  });
 
 
 });
